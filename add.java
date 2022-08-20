3023ef3b6b732d864766f06dc040134165e29e21
siddhuram <goranalesm91@gmail.com>
2022-08-20 08:31:27 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,5 @@
+class add{
+    public static void main(String [] ar){
+System.out.print("opei")
+    }
+}
---
+++ bacic p/index.js
@@ -0,0 +1,7 @@
+function Audi(a,b){
+    var a1=a;
+    var a2=b
+    return a=b
+}
+var ans= Audi(10,38)
+console.log(ans)
